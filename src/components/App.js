4059d120fd4a6847dd1c9b7cf3858ae8bcfc5b5a
FayeZhang2000 <faye.zhang@gophonebox.com>
2024-10-22 17:02:55 -0700

    init

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function App() {
-<<<<<<< HEAD
-  return <div className="App">hello word</div>;
-=======
   return <div className="App">hi</div>;
->>>>>>> 99d61be (initial commit)
 }
 
 export default App;
